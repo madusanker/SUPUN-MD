06ccdcf9f9bd27dd50a219be532fd4ad3b006e9c
madusanker <madusankeishan28@gmail.com>
2025-02-11 15:34:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+kconst fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({
     path: './config.env'
 });
@@ -9,8 +9,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'session id', //add your session id
-MONGODB: process.env.MONGODB || "mongodb url", //add your mongodb url
+SESSION_ID: process.env.SESSION_ID || 'SUPUN-MD=vA9SzYxA#m0-tZTt_8WMo6aRiIAHJNlv18DMfI_dlmk2nqU0bzsU', //add your session id
+MONGODB: process.env.MONGODB || "mongodb://mongo:bBSGMeBojEMPHMWWDvnyTrvswrOcSUDU@autorack.proxy.rlwy.net:15162", //add your mongodb url
  URL : process.env.BOT_MENU_LINKS || 'https://i.ibb.co/bHXBV08/9242c844b83f7bf9.jpg',
 MAX_SIZE: 500
 };
